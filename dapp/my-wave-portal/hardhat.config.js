53b5ebdd5d83ef45b0511164312edd55d1e33af1
krote <krote0924@gmail.com>
2023-01-12 22:57:27 +0900

    first test run test.js

--- dapp/my-wave-portal/hardhat.config.js
+++ dapp/my-wave-portal/hardhat.config.js
@@ -1,4 +1,16 @@
+const { task } = require("hardhat/config");
+
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) =>{
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts){
+    console.log(account.address);
+  }
+});
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
---
+++ dapp/my-wave-portal/test/my-test.js
@@ -0,0 +1,16 @@
+const {expect} = require("chai");
+const hre = require("hardhat");
+const { time } = require("@nomicfoundation/hardhat-network-helpers");
+
+describe("Lock", function(){
+    it("Should set the right unlockTime", async function(){
+        const lockedAmount = 1_000_000_000;
+        const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+        const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+
+        const Lock = await hre.ethers.getContractFactory("Lock");
+        const lock = await Lock.deploy(unlockTime, {value:lockedAmount});
+
+        expect(await lock.unlockTime()).to.equal(unlockTime);
+    });
+});
