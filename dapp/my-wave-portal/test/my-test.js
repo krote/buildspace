6815e0f14f6fe5eec08b5a148eb76cb376ad68df
krote <krote0924@gmail.com>
2023-01-14 22:29:09 +0900

    second test is done

--- dapp/my-wave-portal/test/my-test.js
+++ dapp/my-wave-portal/test/my-test.js
@@ -13,4 +13,15 @@ describe("Lock", function(){
 
         expect(await lock.unlockTime()).to.equal(unlockTime);
     });
+
+    it("Sould revert with the right error if called too soon", async function() {
+        const lockedAmount = 1_000_000_000;
+        const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+        const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+
+        const Lock = await hre.ethers.getContractFactory("Lock");
+        const lock = await Lock.deploy(unlockTime, {value:lockedAmount});
+
+        await expect(lock.withdraw()).to.be.revertedWith("You can't withdraw yet");
+    });
 });
