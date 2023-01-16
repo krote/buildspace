6c329602a40f2d3496f3e5909540da7b6dd495b5
krote <krote0924@gmail.com>
2023-01-16 23:53:16 +0900

    store date on our smart contract

--- dapp/my-wave-portal/test/my-test.js
+++ dapp/my-wave-portal/test/my-test.js
@@ -24,4 +24,17 @@ describe("Lock", function(){
 
         await expect(lock.withdraw()).to.be.revertedWith("You can't withdraw yet");
     });
+
+    it("Should transfer the funds to the owner", async function(){
+        const lockedAmount = 1_000_000_000;
+        const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+        const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+
+        const Lock = await hre.ethers.getContractFactory("Lock");
+        const lock = await Lock.deploy(unlockTime, {value:lockedAmount});
+
+        await time.increaseTo(unlockTime);
+
+        await lock.withdraw();
+    });
 });
