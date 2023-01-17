c6e2b4bf5e9a0aadb1cafcf79e0ce78d763a46aa
krote <krote0924@gmail.com>
2023-01-17 23:53:57 +0900

    change testnet

--- dapp/my-wave-portal/hardhat.config.js
+++ dapp/my-wave-portal/hardhat.config.js
@@ -3,6 +3,8 @@ const { task } = require("hardhat/config");
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
 
+require("dotenv").config();
+
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) =>{
   const accounts = await hre.ethers.getSigners();
 
@@ -15,4 +17,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) =>{
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
