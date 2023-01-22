4f8eed3663a50f8ca7fee02b9b8ae3f7d19037c1
krote <krote0924@gmail.com>
2023-01-22 23:51:50 +0900

    add message param

--- dapp/my-wave-portal/scripts/run.js
+++ dapp/my-wave-portal/scripts/run.js
@@ -1,27 +1,24 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
     console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
 
-    await waveContract.getTotalWaves();
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
 
-    const firstWaveTxn = await waveContract.wave();
-    await firstWaveTxn.wait();
+    /*
+     * Let's send a few waves!
+     */
+    let waveTxn = await waveContract.wave("A message!");
+    await waveTxn.wait();
 
-    await waveContract.getTotalWaves();
+    const [_, randomPerson] = await hre.ethers.getSigners();
+    waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+    await waveTxn.wait();
 
-    const secondWaveTxn = await waveContract.connect(randomPerson).wave();
-    await secondWaveTxn.wait();
-
-    await waveContract.getTotalWaves();
-
-    const thirdWaveTxn = await waveContract.wave();
-    await thirdWaveTxn.wait();
-    await waveContract.getTotalWaves();
-
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
 };
 
 const runMain = async() => {
